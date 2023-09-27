c98738d76230fd45eb9d43a43bd0c7446a79a3a6
Fani <kalamarafani@gmail.com>
2023-09-27 17:06:35 +0200

    Recognition working

--- src/main/java/com/example/harrypottermaze/TranscriberDemo.java
+++ src/main/java/com/example/harrypottermaze/TranscriberDemo.java
@@ -3,10 +3,13 @@ package com.example.harrypottermaze;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 public class TranscriberDemo {
 
@@ -14,24 +17,35 @@ public class TranscriberDemo {
 
         Configuration configuration = new Configuration();
 
-        configuration.setAcousticModelPath(TranscriberDemo.class.getResource("/edu/cmu/sphinx/models/en-us").getPath());
-        configuration.setDictionaryPath(TranscriberDemo.class.getResource("/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict").getPath());
-        configuration.setLanguageModelPath(TranscriberDemo.class.getResource("/edu/cmu/sphinx/models/en-us/en-us.lm.bin").getPath());
+        URL modelBaseURL = TranscriberDemo.class.getResource("/edu/cmu/sphinx/models/en-us/");
+        String modelBasePath = modelBaseURL.toExternalForm();
 
-        /*StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-        InputStream stream = new FileInputStream(new File("test.wav"));
-
-
-               */
+        configuration.setAcousticModelPath(modelBasePath + "en-us");
+        configuration.setDictionaryPath(modelBasePath + "cmudict-en-us.dict");
+        configuration.setLanguageModelPath(modelBasePath + "en-us.lm.bin");
 
         LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
-// Start recognition process pruning previously cached data.
+        //InputStream stream = new FileInputStream(new File("test.wav"));
+
+        recognizer.startRecognition(true);
+        SpeechResult rawResult = recognizer.getResult();
+        if (rawResult != null ) {
+            String hypothesis = rawResult.getHypothesis();
+            System.out.println("Recognized: " + hypothesis);
+        }
+
+        /*
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+        // Start recognition process pruning previously cached data.
         recognizer.startRecognition(true);
         SpeechResult result = recognizer.getResult();
-// Pause recognition process. It can be resumed then with startRecognition(false).
+        // Pause recognition process. It can be resumed then with startRecognition(false).
         recognizer.stopRecognition();
 
-        /*recognizer.startRecognition(stream);
+         */
+
+        /*
+        recognizer.startRecognition(stream);
         SpeechResult result;
         while ((result = recognizer.getResult()) != null) {
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
